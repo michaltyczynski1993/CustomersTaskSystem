4ac87a24873f9e3605de88756938bfb65c1a0374
Michał <m.tyczynski1993@gmail.com>
2022-04-11 13:17:39 +0200

    deleted db and made authorisation by HTTPBasicAuth

--- app.py
+++ app.py
@@ -1,32 +1,17 @@
-import email
-from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    email = db.Column(db.String(120))
-    password = db.Column(db.String(30))
+auth = HTTPBasicAuth()
+users = {
+    "mtyczynski": generate_password_hash("Lolek1200!")
+}
 
 @app.route("/")
 def index():
-    data_list=User.query.all()
-    print(data_list)
     return render_template("index.html")
-@app.route("/add")
-def add():
-    # add new item
-    admin = User(username="michaltyczynski", email="m.tyczynski1993@gmail.com", password="lolek1200")
-    db.session.add(admin)
-    db.session.commit()
-    return redirect(url_for('index'))
 
 @app.route("/about")
 def about():
@@ -40,7 +25,16 @@ def login():
 def signup():
     return render_template("register.html")
 
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+
+@app.route("/todo")
+@auth.login_required
+def todo():
+    username = users
+    return render_template("todo.html", username=username)
+
 if __name__ == "__main__":
-    db.create_all()
-    
     app.run(debug=True)
