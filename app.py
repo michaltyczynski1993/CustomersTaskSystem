00bd87276b7a70565a64c6331df78992551be896
Michał <m.tyczynski1993@gmail.com>
2022-04-12 09:06:50 +0200

    TODO app layout customization

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 users = {
     "mtyczynski": generate_password_hash("Lolek1200!")
 }
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
 
 @app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
 
@@ -17,24 +25,30 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("/signup")
-def signup():
-    return render_template("register.html")
-
 @auth.verify_password
 def verify_password(username, password):
     if username in users and check_password_hash(users.get(username), password):
         return username
 
+"""Routes for Customers TODO app functionality include add, modify and delete functions"""
+
 @app.route("/todo")
 @auth.login_required
 def todo():
     username = users
-    return render_template("todo.html", username=username)
+    todo_list=Todo.query.all()
+    print(todo_list)
+    return render_template("todo.html", username=username, todo_list=todo_list)
+
+@app.route("/add", methods=['POST'])
+def add():
+    # add new item
+    title = request.form.get('title')
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
